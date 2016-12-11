16f005aa973f26fc999a24ac0bc2f3b0aa463bc3
lti <landry.tiehi@gmail.com>
2016-12-11 22:33:42 +0100

    Conf pom.xml and project structure

---
+++ src/main/java/fr/esiee/pic/analytics/Application.java
@@ -0,0 +1,30 @@
+package fr.esiee.pic.analytics;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+/**
+ * Demarrage de l'application
+ * @author etudiant
+ *
+ */
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+	
+	static final Logger logger = LogManager.getLogger(Application.class.getName());
+    
+    /**
+     * Main class
+     */
+    public static void main(String[] args) {
+        logger.info("entered application");
+        
+        SpringApplication.run(Application.class, args);
+    }
+}
+
